74900c498f68f61494d502c65af32ddc8201c238
DH <max16@naver.com>
2024-03-13 15:39:46 +0900

    MarketEye

--- main.py
+++ main.py
@@ -2,28 +2,17 @@ import win32com.client
 
 PATH = "C:\\Users\\max16\\Desktop\\Stocks\\pytrader"
 
-instStockChart = win32com.client.Dispatch("CpSysDib.StockChart")
+instMarketEye = win32com.client.Dispatch("CpSysDib.MarketEye")
+instStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
+code = instStockCode.NameToCode("테크윙")
 
-instStockChart.SetInputValue(0, "A089030") # 종목코드
-instStockChart.SetInputValue(1, ord("1")) # 1: 기간, 2: 갯수
-instStockChart.SetInputValue(2, 20240313) # 종료일
-instStockChart.SetInputValue(3, 20230314) # 시작일
-# instStockChart.SetInputValue(4, 10) # 요청 개수
-instStockChart.SetInputValue(5, (0, 2, 3, 4, 5, 8)) # 필드
-instStockChart.SetInputValue(6, ord("D")) # 차트 구분 D:일
-instStockChart.SetInputValue(9, ord("1"))
+instMarketEye.SetInputValue(0, (4, 67, 70, 111)) # 현재가, PER, EPS, 최근분기년월
+instMarketEye.SetInputValue(1, code)
 
-instStockChart.BlockRequest()
+instMarketEye.BlockRequest()
 
-num_data = instStockChart.GetHeaderValue(3)
 
-# SetInputValue(5, xxx) 에서 요청한 필드의 수
-num_field = instStockChart.GetHeaderValue(1)
-
-print(num_data)
-print(num_field)
-print("일자", "시가", "고가", "저가", "종가", "거래량")
-for i in range(num_data):
-    for j in range(num_field):
-        print(instStockChart.GetDataValue(j, i), end=" ")
-    print("")
+print(f"현재가: {instMarketEye.GetDataValue(0, 0)}")
+print(f"PER: {instMarketEye.GetDataValue(1, 0)}")
+print(f"EPS: {instMarketEye.GetDataValue(2, 0)}")
+print(f"최근분기년월: {instMarketEye.GetDataValue(3, 0)}")
