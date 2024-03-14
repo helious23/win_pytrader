af3a1acf7f192c6e48cf4461f16026cd4664917d
DH <max16@naver.com>
2024-03-14 15:44:22 +0900

    kiwoom login

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QAxContainer import *
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("HelloStock")
-        self.setGeometry(1000, 300, 300, 400)
+        self.setWindowTitle("👋 Hello Stock 📈")
+        self.setGeometry(1000, 300, 300, 150)
         
-        btn1 = QPushButton("Click me", self)
-        btn1.move(20,20)
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        
+        btn1 = QPushButton("Login", self)
+        btn1.move(20, 20)
         btn1.clicked.connect(self.btn1_clicked)
-    
-    def btn1_clicked(self):
-        QMessageBox.about(self, "message", "clicked")
         
+        btn2 = QPushButton("Check state", self)
+        btn2.move(20, 70)
+        btn2.clicked.connect(self.btn2_clicked)
+        
+    def btn1_clicked(self):
+        ret = self.kiwoom.dynamicCall("CommConnect()")
+        
+    def btn2_clicked(self):
+        if self.kiwoom.dynamicCall("GetConnectState()") == 0:
+            self.statusBar().showMessage("접속 안됨😫")
+        else:
+            self.statusBar().showMessage("접속 완료😁")
+            
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mywindow = MyWindow()
-    mywindow.show()
+    my_window = MyWindow()
+    my_window.show()
     app.exec_()
+        
